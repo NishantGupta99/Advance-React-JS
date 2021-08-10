fdf91e0d2a594dc508e6f1c5570e61081e9e6e40
nishant Gupta <kr.nish99@gmail.com>
2021-08-10 22:46:55 +0530

    ..

---
+++ src/tutorial/UseStateBasic.js
@@ -0,0 +1,12 @@
+import React , {useState} from 'react'
+// use state is a named import so we must must must must have the curly braces 
+function UseStateBasic() {
+    console.log(useState);
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default UseStateBasic
